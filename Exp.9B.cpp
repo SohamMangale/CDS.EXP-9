a0709c7a58a384476ce3d57fc8c7b2fc53128c10
SohamMangale <sohammangale60@gmail.com>
2024-10-03 14:56:57 +0530

    Create Exp.9B.cpp

---
+++ Exp.9B.cpp
@@ -0,0 +1,26 @@
+//soham
+//entc B1
+//23070123084
+//experiment 9B
+#include <iostream>
+using namespace std;
+int main()
+{
+    int *p ,b=10;
+    p = &b ;
+    cout <<*p << "  " << b << endl <<p <<"  "<< &b<<endl;
+    p++;
+    cout<<"After increment:" <<p<<endl;
+    float *n, a=8.923;
+    n=&a;
+    cout<< endl<<*n<<"  "<<a<<endl<<n<<"  "<<&a<<endl;
+    n++;
+    cout<<" After increment" <<n <<endl;
+    char *ch, c(10);
+    c='#';
+    ch=&c;
+    cout<< endl<< (void*)ch<<"  "<<endl;
+    ch++;
+    cout<<" After increment:" << (void*)ch<<endl;
+    return 0;
+}
